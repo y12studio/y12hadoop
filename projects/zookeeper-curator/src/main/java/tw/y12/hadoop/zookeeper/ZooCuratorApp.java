b5288c6ad7d05ce9284becca89381ed9fdb49410
y12studio <y12studio@gmail.com>
2014-04-21 17:45:38 +0800

    remove log4j #7

--- projects/zookeeper-curator/src/main/java/tw/y12/hadoop/zookeeper/ZooCuratorApp.java
+++ projects/zookeeper-curator/src/main/java/tw/y12/hadoop/zookeeper/ZooCuratorApp.java
@@ -15,10 +15,20 @@
  */
 package tw.y12.hadoop.zookeeper;
 
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
 public class ZooCuratorApp {
 
 	public static void main(String[] args) {
 		System.out.println("hello zookeeper/curator");
+		RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+		String zkconn = "127.0.0.1:2181";
+		CuratorFramework client = CuratorFrameworkFactory.newClient(zkconn,
+				retryPolicy);
+		client.start();
 
 	}
 
